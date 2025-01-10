f383b8c9f25143c038e9c587a674b322567209ff
Parsonss1234 <parsonss123@gmail.com>
2025-01-10 13:20:06 -0700

    DEI Awareness

---
+++ story_content/609ZZzyGxzf_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.93.33359.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:03.362%0D%0AModule%203:%20Overcoming%20Bias%20and%20Stereotypes.%0D%0A%0D%0A"}')
